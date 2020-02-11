066ca0f634ad8b67f095783376b9b76cf63e854b
avaakash <as86414@gmail.com>
2020-02-11 23:19:47 +0530

    db reset

--- crypto/settings.py
+++ crypto/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'crypto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crypto_db',
-        'USER': 'crypto_user',
-        'PASSWORD': 'rajbabu007',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
